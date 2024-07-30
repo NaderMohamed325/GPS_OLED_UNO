333c2feb1af9e803531adc8f685a92eb968d261b
Nader Mohamed Nader <nadoorclasher@gmail.com>
2024-07-30 22:28:06 +0300

    Update gps_copy_20240730210350.ino

--- gps_copy_20240730210350/gps_copy_20240730210350.ino
+++ gps_copy_20240730210350/gps_copy_20240730210350.ino
@@ -9,7 +9,7 @@ void loop() {
   float latitude = random(-90, 90) + random(0, 1000000) / 1000000.0;
   float longitude = random(-180, 180) + random(0, 1000000) / 1000000.0;
   
-  // NMEA sentence format: $GPGGA,hhmmss.ss,latitude,lat_dir,longitude,lon_dir,fix_quality,num_satellites,hdop,altitude,units,geoidal_separation,units,dgps_age,dgps_station_id*checksum
+  
   String gpsData = "$GPGGA,123519,";
 
   // Format latitude
